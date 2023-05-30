8a45c62c437ed28120a541fdbbb3cd4b3e058714
yumin <cfy113@naver.com>
2023-05-31 01:26:13 +0900

    Update

--- thisisjava/src/collection_practice/list_practice/practice03/domain/Biz.java
+++ thisisjava/src/collection_practice/list_practice/practice03/domain/Biz.java
@@ -1,30 +1,20 @@
 package collection_practice.list_practice.practice03.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 public class Biz {
 	
 	private String[] contents;
 	
 	public List<Book> convert(String[] contents) {
-		StringTokenizer st;
-		List<Book> bookList = new ArrayList<>();
-		
-		for(String data : contents) {
-			st = new StringTokenizer(data, "/");
-			String category = st.nextToken();
-			String title = st.nextToken();
-			String writer = st.nextToken();
-			String publisher = st.nextToken();
-			int price = Integer.parseInt(st.nextToken());
-			String isbn = st.nextToken();
-			
-			Book book = new Book(category, title, writer, publisher, price, isbn);
-			bookList.add(book);
-		}
-		return bookList;
+		return Arrays
+				.stream(contents)
+				.map(it -> Book.of(new StringTokenizer(it, "/")))
+				.collect(Collectors.toList());	
 	}
-
 }
--- thisisjava/src/collection_practice/list_practice/practice03/domain/Book.java
+++ thisisjava/src/collection_practice/list_practice/practice03/domain/Book.java
@@ -1,5 +1,7 @@
 package collection_practice.list_practice.practice03.domain;
 
+import java.util.StringTokenizer;
+
 public class Book {
 	
 	private String category;
@@ -9,13 +11,107 @@ public class Book {
 	private int price;
 	private String isbn;
 	
-	public Book(String category, String title, String writer, String publisher, int price, String isbn) {
-		this.category = category;
-		this.title = title;
-		this.writer = writer;
-		this.publisher = publisher;
-		this.price = price;
-		this.isbn = isbn;
+	public static Book of(String category, String title, String writer, String publisher, int price, String isbn) {
+		return Book.builder()
+				.isbn(isbn)
+				.category(category)
+				.title(title)
+				.publisher(publisher)
+				.writer(writer)
+				.price(price)
+				.build();
+	}
+	
+	public static Book of(StringTokenizer st) {	
+		return Book.builder()
+				.category(st.nextToken())
+				.title(st.nextToken())
+				.writer(st.nextToken())
+				.publisher(st.nextToken())
+				.price(Integer.parseInt(st.nextToken()))
+				.isbn(st.nextToken())
+				.build();
+	}
+	
+	public Book(Builder builder) {
+		this.category = builder.category;
+		this.title = builder.title;
+		this.writer = builder.writer;
+		this.publisher = builder.publisher;
+		this.price = builder.price;
+		this.isbn = builder.isbn;
+	}
+	
+	public static Builder builder() {
+		return new Builder();
+	}
+	
+	static class Builder {
+		private String category;
+		private String title;
+		private String writer;
+		private String publisher;
+		private int price;
+		private String isbn;
+		
+		public Builder category(String category) {
+			this.category = category;
+			return this;
+		}
+		
+		public Builder title(String title) {
+			this.title = title;
+			return this;
+		}
+		
+		public Builder writer(String writer) {
+			this.writer = writer;
+			return this;
+		}
+		
+		public Builder publisher(String publisher) {
+			this.publisher = publisher;
+			return this;
+		}
+		
+		public Builder price(int price) {
+			this.price = price;
+			return this;
+		}
+		
+		public Builder isbn(String isbn) {
+			this.isbn = isbn;
+			return this;
+		}
+		
+		public String getCategory() {
+			return this.category;
+		}
+		
+		public String getTitle() {
+			return this.title;
+		}
+		
+		public String getWriter() {
+			return this.writer;
+		}
+		
+		public String getPublisher() {
+			return this.publisher;
+		}
+		
+		public int getPrice() {
+			return this.price;
+		}
+		
+		public String getIsbn() {
+			return this.isbn;
+		}
+		
+		public Book build() {
+			return new Book(this);
+		}
+		
 	}
 
 	public String getCategory() {
