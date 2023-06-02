0a629cab7c5688e4bcc46015c7bdf366c1570a42
Yumin <cfy113@naver.com>
2023-06-02 18:05:08 +0900

    Update

---
+++ thisisjava/src/network_practice/chat_program/socket_client/SocketClient.java
@@ -0,0 +1,41 @@
+package network_practice.chat_program.socket_client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import network_practice.chat_program.chat_server.ChatServer;
+
+public class SocketClient {
+
+	ChatServer chatServer;
+	Socket socket;
+	DataInputStream dis;
+	DataOutputStream dos;
+	String clientIp;
+	String chatName;
+	
+	public SocketClient(ChatServer chatServer, Socket socket) {
+		try {
+			this.chatServer = chatServer;
+			this.socket = socket;
+			this.dis = new DataInputStream(socket.getInputStream());
+			this.dos = new DataOutputStream(socket.getOutputStream());
+			InetSocketAddress isa = (InetSocketAddress) socket.getRemoteSocketAddress();
+			this.clientIp = isa.getHostName();
+			receive();
+		} catch(IOException e) {
+			
+		}
+	}
+	
+	public void receive() {
+		chatServer.threadPool.execute(() -> {
+			
+		})
+	}
+	
+	
+}
